a27ea0d86124e3c4f6d2cd1e4922ed328af35a4b
Noah Sano <noahcsano@berkeley.edu>
2023-08-11 13:50:43 -0700

    changed features of graph

--- graphing.js
+++ graphing.js
@@ -1,6 +1,12 @@
 //Get container for graph
 var container = document.getElementById('desmos');
-var calculator = Desmos.GraphingCalculator(container, {keypad: false, expressions: false, settingsMenu: false, invertedColors: true});
+var calculator = Desmos.GraphingCalculator(container, {expressionsCollapsed: true, keypad: false, settingsMenu: false, invertedColors: true});
+
+document.getElementsByClassName("dcg-left-buttons")[0].setAttribute("id", "hideStuff");
+
+//event listener for arrow key
+
+
 // Event listener for graph button
 var enter = document.getElementById('enter');
 var equation = document.getElementById('equation');
@@ -10,13 +16,12 @@ enter.addEventListener('click', () => {
     graphEquation(eq);
 });
 
-
 function graphEquation(equation) {
     // Clear existing expressions
     calculator.setBlank();
 
     // Add expression to calculator
-    calculator.setExpression({ id: 'graph1', latex: equation });
+    calculator.setExpression({ id: 'graph1', latex: equation});
 
     // Update the graph
     calculator.updateSettings({ left: -10, right: 10, top: 10, bottom: -10 });
@@ -28,3 +33,6 @@ document.addEventListener("keydown", function(event) {
         window.location.href = "index.html";
     }
 });
+GraphingCalculator.resize()
+    document.getElementsByClassName("dcg-left-buttons")[0].setAttribute("id", "hideStuff");
+
